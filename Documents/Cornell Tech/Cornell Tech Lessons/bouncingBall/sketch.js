7af42f257ebedbb2b14da461edec7de16a506bfc
Michelle Hecht <michellehecht@michelles-imac.home>
2016-09-29 21:49:57 -0400

    first time

---
+++ Documents/Cornell Tech/Cornell Tech Lessons/bouncingBall/sketch.js
@@ -0,0 +1,26 @@
+// bouncing ball 3.2
+var x = 0;
+var speed = 3;
+
+function setup() {
+  createCanvas(600, 400);
+ 
+}
+
+function draw() {
+  background(0);
+  
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  
+  if (x > width) {
+    speed = -3;
+  }
+  
+  if (mouseX > 300) {
+    fill(255, 0, 200);
+  } 
+    ellipse(x, 200, 100, 100)
+    x = x + speed;
+}
